567aba5479da7abcd58b5e87e098b2f0e999d45d
hamitmizrak <hamitmizrak@gmail.com>
2022-12-11 20:21:39 +0300

    Firebase config

--- app/src/main/java/com/hamitmizrak/Deneme.java
+++ app/src/main/java/com/hamitmizrak/Deneme.java
@@ -1,11 +1,27 @@
 package com.hamitmizrak;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 
-@Data
+@Getter @Setter
 public class Deneme {
     private String name;
     private String surname;
+
+
+    //1.ADIM
+   /* dependencies {
+        compileOnly 'org.projectlombok:lombok:1.18.24'
+        annotationProcessor 'org.projectlombok:lombok:1.18.24'
+        */
+
+    //2.ADIM
+    //https://plugins.jetbrains.com/plugin/6317-lombok/versions#tabs
+
+    //3.ADIM
+    //C:\Program Files\Android\Android Studio\plugins  => BURAYA EKLE
 
 }
